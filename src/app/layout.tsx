814fe4eb45f946d14e22c0a918077b03a0fe8dea
TalhaBayansar <talha.bayansar@ordina.be>
2023-11-22 01:50:32 +0100

    removing export for fontSans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { TRPCReactProvider } from "~/trpc/react";
 import { NextAuthProvider } from "~/components";
 import { cn } from "~/lib";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
