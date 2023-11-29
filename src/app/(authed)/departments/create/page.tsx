437ba3b56d17469c6cd9811089da044f6b34c94a
Talha-Bayansar <talha.bayansar@hotmail.com>
2023-11-30 00:50:04 +0100

    implemented create functionality to create department page

--- src/app/(authed)/departments/create/page.tsx
+++ src/app/(authed)/departments/create/page.tsx
@@ -1,11 +1,27 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { PageWrapper } from "~/components";
 import { DepartmentsForm } from "~/departments";
+import { routes } from "~/lib";
+import { api } from "~/trpc/react";
 
 const Page = () => {
+  const router = useRouter();
+  const mutation = api.departments.create.useMutation({
+    onSuccess: () => {
+      router.replace(routes.departments);
+    },
+  });
+
   return (
-    <PageWrapper className="md:max-w-lg" title="New department">
-      <DepartmentsForm onSubmit={console.log} isSubmitting={false} />
+    <PageWrapper
+      className="flex flex-grow flex-col md:max-w-lg"
+      title="New department"
+    >
+      <DepartmentsForm
+        onSubmit={mutation.mutate}
+        isSubmitting={mutation.isLoading}
+      />
     </PageWrapper>
   );
 };
