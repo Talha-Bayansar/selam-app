8a39673bcba6707b5f3872cb092a2c9387357920
Talha-Bayansar <talha.bayansar@hotmail.com>
2023-12-13 16:23:47 +0100

    refactoring unauthenticated component

--- src/components/Unauthenticated.tsx
+++ src/components/Unauthenticated.tsx
@@ -1,3 +1,4 @@
+import { Lock } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { routes } from "~/lib";
@@ -6,15 +7,17 @@ export const Unauthenticated = () => {
   const pathName = usePathname();
 
   return (
-    <div className="place-items-centerp-4 grid h-screen place-content-center">
+    <div className="grid h-screen place-content-center place-items-center gap-8 p-8">
+      <Lock className="text-destructive" size={80} />
       <p className="text-center">
-        It looks like you are not authenticated.
+        It looks like you are not authenticated.{" "}
         <Link
           href={`${routes.signIn}?callbackUrl=${pathName}`}
-          className="cursor-pointer text-blue-600"
+          className="cursor-pointer text-primary underline"
         >
           Authenticate
-        </Link>
+        </Link>{" "}
+        to continue.
       </p>
     </div>
   );
